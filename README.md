8e6010f87ea1cbec0cbc17b73068b7f9cb906c9e
Mohammad Javad Javadi <mohammadjavad.javadir@gmail.com>
2023-08-30 17:10:28 +0330

    First Commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # flutter_application_1
-
+### This is My First Flutter App
 A new Flutter project.
 
 ## Getting Started
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(Application());
+}
+
+Widget Application() {
+  return MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.amber,
+      body: SafeArea(
+        child: Text('Hello This is App'),
+      ),
+    ),
+  );
+}
